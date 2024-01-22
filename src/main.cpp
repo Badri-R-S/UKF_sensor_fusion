7a1d0ab7fef8960bd76c738d0448a38e4857880a
Badri-R-S <81817254+Badri-R-S@users.noreply.github.com>
2024-01-22 04:20:17 -0500

    Program that communicates with the Term 2 Simulator receiving data measurements, calls a function to run the Kalman filter, calls a function to calculate RMSE

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main()
   Tools tools;
   vector<VectorXd> estimations;
   vector<VectorXd> ground_truth;
-
+ 
   h.onMessage([&ukf,&tools,&estimations,&ground_truth](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
